b988049720fa5afb0beb056086098de087e8fa9e
xianglishan <91013631+xianglishan@users.noreply.github.com>
2024-01-26 12:11:08 +0000

    a

--- setup_githooks.sh
+++ setup_githooks.sh
@@ -20,16 +20,16 @@ for file in \$(git diff --cached --name-only); do
     ## ipynbの場合
     if [[ \$file == *.ipynb ]]; then
         ## delete output
-        jupyter nbconvert --ClearOutputPreprocessor.enabled=True \
-            --ClearOutputPreprocessor.remove_metadata_fields=[]  \
-            --ClearMetadataPreprocessor.enabled=True  \
-            --ClearMetadataPreprocessor.preserve_nb_metadata_mask='[("language_info"),("kernelspec")]' \
+        jupyter nbconvert --ClearOutputPreprocessor.enabled=True \\
+            --ClearOutputPreprocessor.remove_metadata_fields=[]  \\
+            --ClearMetadataPreprocessor.enabled=True  \\
+            --ClearMetadataPreprocessor.preserve_nb_metadata_mask='[("language_info"),("kernelspec")]' \\
             --to notebook --inplace \${file}
 
         ## save output
-        # jupyter nbconvert \
-            # --ClearMetadataPreprocessor.enabled=True  \
-            # --ClearMetadataPreprocessor.preserve_nb_metadata_mask='[("language_info"),("kernelspec")]' \
+        # jupyter nbconvert \\
+            # --ClearMetadataPreprocessor.enabled=True  \\
+            # --ClearMetadataPreprocessor.preserve_nb_metadata_mask='[("language_info"),("kernelspec")]' \\
             # --to notebook --inplace \${file}
 
         ## ipynb -> py script
